098b945be097c3ac392723dd3490428d7c4470d5
dilap54 <dilap54@mail.ru>
2018-05-12 18:25:01 +0300

    Фиксы CORS

--- app/index.js
+++ app/index.js
@@ -4,6 +4,19 @@ const app = express();
 const sequelize = require('./sequelize');
 const api = require('./api');
 
+app.options("/*", function(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, X-Auth-Token');
+    res.sendStatus(200);
+});
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, DELETE, OPTIONS');
+    next();
+})
+
 app.use('/apidoc', express.static('apidoc'));
 
 app.use('/api', api);
